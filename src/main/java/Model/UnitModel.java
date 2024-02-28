15cfe08ae4fbf2b463242bb2cb6f067131e028d5
tranhoangquy98917 <160830020+tranhoangquy98917@users.noreply.github.com>
2024-02-28 11:03:44 +0700

    Add files via upload

---
+++ src/main/java/Model/UnitModel.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author User
+ */
+public class UnitModel {
+    private int UnitID; 
+    private String UnitName;
+
+    public UnitModel() {
+    }
+
+    public UnitModel(int UnitID, String UnitName) {
+        this.UnitID = UnitID;
+        this.UnitName = UnitName;
+    }
+
+    public int getUnitID() {
+        return UnitID;
+    }
+
+    public void setUnitID(int UnitID) {
+        this.UnitID = UnitID;
+    }
+
+    public String getUnitName() {
+        return UnitName;
+    }
+
+    public void setUnitName(String UnitName) {
+        this.UnitName = UnitName;
+    }
+    
+}
